c3454e916bb19cb7d23c7351a14fb50db001c3b8
Jonathan <jonsouyang@gmail.com>
2025-11-19 17:37:47 -0800

    removed 3D asssets

--- src/App.jsx
+++ src/App.jsx
@@ -19,59 +19,58 @@ const App = () => {
       <Routes>
         <Route path="/" element={ // homepage
           <main className="bg-black">
-          <Navbar />
-          <Hero />
-          <Highlights />
-          <Features />
-          <HowItWorks/>
-          <Footer/>
+            <Navbar />
+            <Hero />
+            <Highlights />
+            <Features />
+            <HowItWorks/>
+            <Footer/>
          </main>
         } />
         <Route path="/test-home" element={ // test dev homepage
           <main className="bg-black">
-          <Navbar />
-          <Opener />
-          <Highlights />
-          <Features />
-          <HowItWorks/>
-          <Footer />
+            <Navbar />
+            <Opener />
+            <Highlights />
+            <Features />
+            <HowItWorks/>
+            <Footer />
          </main>
         } />
         <Route path="/work-history" element={ // work history page
           <main className="bg-black">
-          <Navbar />
-          <GlowingHeader>Experience</GlowingHeader>
-          <h1 style={{ textAlign: 'center', color: '#e2e8f0' }}>
-            Professional Work History. For Research history, please view the Research page.
-          </h1>
-          <ExperienceGraph
-            experiences={experienceData.experiences}
-            branches={experienceData.branches}
-          />
+            <Navbar />
+            <GlowingHeader>Experience</GlowingHeader>
+            <h1 style={{ textAlign: 'center', color: '#e2e8f0' }}>
+              Professional Work History. For Research history, please view the Research page.
+            </h1>
+            <ExperienceGraph
+              experiences={experienceData.experiences}
+              branches={experienceData.branches}
+            />
          </main>
         } />
         <Route path="/research" element={ // research page
           <main className="bg-black">
-          <Navbar />
-          <GlowingHeader>Research</GlowingHeader>
-          {/* <Timeline experiences={researchData} /> */}
-          <ResearchPage></ResearchPage>
+            <Navbar />
+            <GlowingHeader>Research</GlowingHeader>
+            <ResearchPage></ResearchPage>
          </main>
         } />
         <Route path="/contact" element={ // contact page
           <main className="bg-black">
-          <Navbar />
-          <GlowingHeader>Contacts</GlowingHeader>
+            <Navbar />
+            <GlowingHeader>Contacts</GlowingHeader>
          </main>
         } />
         <Route path="/beta" element={ // beta testing page
           <main className="bg-black">
-          <Navbar />
-          <Opener />
-          <Highlights />
-          <Features />
-          <HowItWorks/>
-          <Footer />
+            <Navbar />
+            <Opener />
+            <Highlights />
+            <Features />
+            <HowItWorks/>
+            <Footer />
          </main>
         } />
       </Routes>
